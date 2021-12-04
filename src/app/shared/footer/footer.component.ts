ca4a0c693aef9df80d0bc1cc96392cfd658ade24
Khushal Rawal <khushalfluxbyte@gmai.com>
2021-12-04 14:17:04 +0530

    No change

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  @ViewChild('hrHand', { static: false })
+  hrHand!: ElementRef;
+  @ViewChild('minHand', { static: false })
+  minHand!: ElementRef;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(()=>{
+      const date = new Date();
+      this.updateClock(date);
+    }, 1000);
   }
+    updateClock(date: Date){
+      this.minHand.nativeElement.style.transform = 'rotate(' + date.getMinutes() *6 + 'deg)';
+      this.hrHand.nativeElement.style.transform = 'rotate(' + (date.getHours() *30 + date.getMinutes()*0.5)+ 'deg)'; 
+   }
 
 }
