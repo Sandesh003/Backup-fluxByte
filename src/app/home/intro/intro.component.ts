62467111874e04d68c851b934ba552935bafb8c1
Sandesh Ghone <sandesh.fluxbyte@gmail.com>
2022-02-28 12:08:55 +0530

    home page animation almost done

--- src/app/home/intro/intro.component.ts
+++ src/app/home/intro/intro.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, HostListener, AfterViewInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
-  selector: 'app-intro',
-  templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+	selector: 'app-intro',
+	templateUrl: './intro.component.html',
+	styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent implements OnInit {
+export class IntroComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+	constructor(private el: ElementRef) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	ngAfterViewInit(): void {
+		const tl = gsap.timeline();
+		tl.fromTo(".slide-in-right", {x: -200, opacity: 0,duration: 1},{x: 0, opacity: 1,duration:1})
+		  .fromTo(".slide-in-left",{x: 150, opacity: 0},{x:0, opacity: 1}, "<50%")
+
+		ScrollTrigger.create({
+			animation: tl,
+			trigger: ".slider",
+			start: "top top",
+			end: "top -2%",
+			scrub:2
+		})
+	}
+
+	scrollElements = this.el.nativeElement.querySelectorAll('.js-scroll');
+
+	elementInView = (el: any, dividend = 1) => {
+		const elementTop = el.getBoundingClientReact().top;
+		console.log('im in scroll function',elementTop);
+		return (
+			elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+		);
+	} 
+
+	elementOutofView = (el: any) => {
+		const elementTop = el.getBoundingClientRect().top;
+		console.log(elementTop)
+		return (
+			elementTop > (window.innerHeight || document.documentElement.clientHeight)
+		);
+	};
+
+	displayScrollElement = (element: any) => {
+		element.classList.add("scrolled");
+	};
+
+	hideScrollElement = (element: any) => {
+		element.classList.remove("scrolled");
+	};
+
+	@HostListener('document:scroll')
+	scrollfunction() {
+		
+		console.log('i am el on hostlistener')
+		// debugger
+		this.scrollElements.forEach((el: any) => {
+			console.log(el, 'i am el')
+			if (this.elementInView(el, 1.2)) {
+				this.displayScrollElement(el);
+				console.log('im in if');
+				
+			} else if (this.elementOutofView(el)) {
+				this.hideScrollElement(el);
+			}
+		})
+	};
 }
--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent implements OnInit {
-
+export class ServicesComponent implements OnInit, AfterViewInit {
+	@ViewChild("text") text!:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+	const letters = this.text.nativeElement.innerHTML.split("")
+	this.text.nativeElement.innerHTML = "";
+	letters.forEach((letter:string)=> {
+		this.text.nativeElement.innerHTML += `<span class="letter">${letter}</span>`
+	});
+	gsap.set(".letter",{display: "inline-block"});
+	const letterClass = gsap.fromTo(".letter",{y:30},{y:0, stagger: 0.1})
+	ScrollTrigger.create({
+		animation: letterClass,
+		trigger:".section",
+		start: "top 40%",
+		end: "top 35%",
+		scrub: 2
+	})
+	  const tl = gsap.timeline();
+	  tl.fromTo(".section-title",{ scale: 1.3, opacity: 0 },{	scale: 1, opacity: 1, delay: 0.35, duration: 2.5, ease: "elastic.out(1.5,1)" })
+	  	// .fromTo(".h6", {duration: 1, opacity: 0},{duration: 1, opacity: 1},"<55%")
+		.fromTo("#box1, #box4", {x: -100, opacity: 0},{x: 0, opacity: 1},"<55%")
+		.fromTo("#box2", {y: -200, opacity: 0},{y: 0, opacity: 1},"<55%")
+		.fromTo("#box3, #box6", {x: 100, opacity: 0},{x: 0, opacity: 1},"<55%")
+		.fromTo("#box5", {y: 200, opacity: 0}, {y: 0, opacity: 1},"<55%")
+
+	ScrollTrigger.create({
+		animation:tl,
+		trigger:".section",
+		start: "top 50%",
+		end: "top top",
+		scrub:2
+	})
+
+	
   }
 
 }
